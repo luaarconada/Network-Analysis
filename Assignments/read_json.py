5256f2230fb9215a91e249d2f1bc2342c37473a8
luaarconada <luaarconada@gmail.com>
2024-04-24 12:44:54 +0200

    Assignment 2 and 3 and used files

---
+++ Assignments/read_json.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 11:53:08 2024
+
+@author: alema
+"""
+
+import json
+
+f = open("congress_network/congress_network/congress_network_data.json")
+
+data = json.load(f)
+
+f.close()
